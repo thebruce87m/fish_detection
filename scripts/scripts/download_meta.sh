b397d54c247ea4344fce29aa324a0f82ed3c25bd
Brian Lee <brian6@gmail.com>
2020-08-14 22:32:09 +0100

    Script to download meta data

---
+++ scripts/scripts/download_meta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+wget -nc https://storage.googleapis.com/openimages/2018_04/train/train-annotations-bbox.csv -P /opt/data/meta
+wget -nc https://storage.googleapis.com/openimages/2018_04/validation/validation-annotations-bbox.csv -P /opt/data/meta
+wget -nc https://storage.googleapis.com/openimages/2018_04/test/test-annotations-bbox.csv -P /opt/data/meta
+wget -nc https://storage.googleapis.com/openimages/2018_04/class-descriptions-boxable.csv -P /opt/data/meta
